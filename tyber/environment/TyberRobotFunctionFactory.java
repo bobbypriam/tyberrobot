b1238f7ee8792c93f96db759df7bcd3484ecab41
Bobby Priambodo <bobby.priambodo@gmail.com>
2014-10-04 00:00:47 +0700

    Handled move actions in ResultFunction

--- tyber/environment/TyberRobotFunctionFactory.java
+++ tyber/environment/TyberRobotFunctionFactory.java
@@ -2,10 +2,12 @@ package tyber.environment;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.StringTokenizer;
 
 import aima.core.agent.Action;
 import aima.core.search.framework.ActionsFunction;
 import aima.core.search.framework.ResultFunction;
+import aima.core.util.datastructure.XYLocation;
 
 public class TyberRobotFunctionFactory {
   private static ActionsFunction _actionsFunction = null;
@@ -31,10 +33,14 @@ public class TyberRobotFunctionFactory {
 
       Set<Action> actions = new LinkedHashSet<Action>();
 
-      // to be implemented
-      // possible actions:
-      // 1. move: up, down, left, right
-      // 2. sweep: up-up, up-down, up-left, up-right, etc.
+      XYLocation robotOne = board.getRobotOne();
+      XYLocation robotTwo = board.getRobotTwo();
+      List<XYLocation> robotOneSurroundings = board.getRobotSurroundings(robotOne);
+      List<XYLocation> robotTwoSurroundings = board.getRobotSurroundings(robotTwo);
+
+      Set<XYLocation> dusts = board.getDusts();
+      for (XYLocation dust : dusts)
+        if (isBeside(robotOne, dust))
 
       return actions;
     }
@@ -45,10 +51,41 @@ public class TyberRobotFunctionFactory {
       if (a instanceof RobotAction) {
         RobotAction ra = (RobotAction) a;
         TyberRobotBoard board = (TyberRobotBoard) s;
-        TyberRobotBoard newBoard = new TyberRobotBoard(board.getN(), board.getM());
+        TyberRobotBoard newBoard = new TyberRobotBoard(board);
 
-        // to be implemented
-        // resulting states after action a
+        XYLocation robotOne = board.getRobotOne();
+        XYLocation robotTwo = board.getRobotTwo();
+
+        StringTokenizer tok = new StringTokenizer(ra.getName());
+
+        String robotOneAction = tok.nextToken();
+        String robotTwoAction = tok.nextToken();
+
+        // Handle move actions.
+        XYLocation newRobotOne = robotOne;
+        XYLocation newRobotTwo = robotTwo;
+
+        if (robotOneAction.equals(RobotAction.MOVE_UP))
+          newRobotOne = board.getFinalLocation(robotOne, TyberRobotBoard.UP);
+        else if (robotOneAction.equals(RobotAction.MOVE_DOWN))
+          newRobotOne = board.getFinalLocation(robotOne, TyberRobotBoard.DOWN);
+        else if (robotOneAction.equals(RobotAction.MOVE_LEFT))
+          newRobotOne = board.getFinalLocation(robotOne, TyberRobotBoard.LEFT);
+        else if (robotOneAction.equals(RobotAction.MOVE_RIGHT))
+          newRobotOne = board.getFinalLocation(robotOne, TyberRobotBoard.RIGHT);
+
+        if (robotTwoAction.equals(RobotAction.MOVE_UP))
+          newRobotTwo = board.getFinalLocation(robotTwo, TyberRobotBoard.UP);
+        else if (robotTwoAction.equals(RobotAction.MOVE_DOWN))
+          newRobotTwo = board.getFinalLocation(robotTwo, TyberRobotBoard.DOWN);
+        else if (robotTwoAction.equals(RobotAction.MOVE_LEFT))
+          newRobotTwo = board.getFinalLocation(robotTwo, TyberRobotBoard.LEFT);
+        else if (robotTwoAction.equals(RobotAction.MOVE_RIGHT))
+          newRobotTwo = board.getFinalLocation(robotTwo, TyberRobotBoard.RIGHT);
+
+        // Place robots in new positions.
+        newBoard.putElement(newRobotOne, TyberRobotBoard.ROBOT_ONE);
+        newBoard.putElement(newRobotTwo, TyberRobotBoard.ROBOT_TWO);
 
         s = newBoard;
       }
