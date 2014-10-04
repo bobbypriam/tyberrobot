22139fb3a7df358a0ee54dae25b8d82f5aaf2687
Bobby Priambodo <bobby.priambodo@gmail.com>
2014-10-04 21:11:33 +0700

    Deleted unnecessary actions (UD, DU, LR, RL)

--- tyber/environment/RobotAction.java
+++ tyber/environment/RobotAction.java
@@ -10,11 +10,9 @@ public class RobotAction extends DynamicAction {
   public static final String MOVE_RIGHT = "R";
 
   public static final String SWEEP_UP_UP = "UU";
-  public static final String SWEEP_UP_DOWN = "UD";
   public static final String SWEEP_UP_LEFT = "UL";
   public static final String SWEEP_UP_RIGHT = "UR";
 
-  public static final String SWEEP_DOWN_UP = "DU";
   public static final String SWEEP_DOWN_DOWN = "DD";
   public static final String SWEEP_DOWN_LEFT = "DL";
   public static final String SWEEP_DOWN_RIGHT = "DR";
@@ -22,11 +20,9 @@ public class RobotAction extends DynamicAction {
   public static final String SWEEP_LEFT_UP = "LU";
   public static final String SWEEP_LEFT_DOWN = "LD";
   public static final String SWEEP_LEFT_LEFT = "LL";
-  public static final String SWEEP_LEFT_RIGHT = "LR";
 
   public static final String SWEEP_RIGHT_UP = "RU";
   public static final String SWEEP_RIGHT_DOWN = "RD";
-  public static final String SWEEP_RIGHT_LEFT = "RL";
   public static final String SWEEP_RIGHT_RIGHT = "RR";
 
   /**
